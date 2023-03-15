0a34f5c03231fef43d2089e5d54717e12e62aed7
Noriyuki Matsumoto <matsunori39@gmail.com>
2023-03-15 11:11:39 +0900

    Add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80, // Number of characters displayed per line
+  tabWidth: 2,  // Indent Size
+  useTabs: false, // Whether to use tabs instead of spaces for indentation
+  semi: true, // Whether to put a semicolon after a statement
+  singleQuote: false, // Whether to enclose the string in single quotes or not (double quotes if "false")
+  quoteProps: "as-needed",  // Whether to enclose object property names in quotes
+  jsxSingleQuote: false,  // Whether to enclose quotes in JSX with single quotes
+  trailingComma: "es5", // Whether to add trailing commas for multiple lines
+  bracketSpacing: true, // Whether to include spaces before and after "{}" in object literals
+  bracketSameLine: false, // Whether to include the closing tag of the element in the JSX in the last line
+  arrowParens: "always",  // Whether to enclose an Arrow function with parentheses when it has a single argument
+};
